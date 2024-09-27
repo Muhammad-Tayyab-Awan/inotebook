9ad108af62acaa29f038d8e296b7816704e42f4e
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-27 16:07:14 +0500

    Edited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,11 @@
 import Notes from "./Notes";
 
 function Home() {
-  return <Notes />;
+  return (
+    <div className="min-h-[calc(100vh-9.5rem)] bg-yellow-500 pt-16 pb-10">
+      <Notes />
+    </div>
+  );
 }
 
 export default Home;
