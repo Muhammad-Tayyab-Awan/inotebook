0d3780ee0cb6276773ff80f8eef885d905305514
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-27 08:44:04 +0500

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import About from "./components/About.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
