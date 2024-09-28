7d18784d51cd2310ce2ef9cf78e9efea14e0fd4e
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-28 19:17:57 +0500

    added succes status in responses

--- backend/middleware/getUser.js
+++ backend/middleware/getUser.js
@@ -1,15 +1,20 @@
 import JWT from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET;
 function getUser(req, res, next) {
-  const token = req.header("auth-token");
+  let token = req.header("auth-token");
   if (!token) {
-    res.status(401).json({ Error: "Access denied from server!" });
+    res
+      .status(401)
+      .json({ success: false, error: "Access denied from server!" });
   } else {
-    JWT.verify(token, JWT_SECRET, (err, user) => {
+    JWT.verify(token, JWT_SECRET, (err, response) => {
       if (err) {
-        res.status(403).json({ error: "Token is not valid!" });
+        res.status(403).json({
+          status: false,
+          error: "Token is not valid!"
+        });
       } else {
-        req.user = user;
+        req.user = response;
         next();
       }
     });
