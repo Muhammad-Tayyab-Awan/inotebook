e9e1f2e324e29d316c18cb67fbd39de145c563b8
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-28 07:24:30 +0500

    added cors setup

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,18 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 const app = express();
-const port = 3000;
+const port = 8080;
 import dbConnection from "./dbConnection.js";
 import authRoute from "./routes/auth.js";
 import notesRoute from "./routes/notes.js";
 dbConnection();
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
