7dcf86ee193dc82f48b491feefcfbb34ad8737a4
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-28 19:10:36 +0500

    Added Redirect funtionality and condition for no notes

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,12 +1,18 @@
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import NotesContext from "../context/notes/NotesContext";
 import Noteitem from "./Noteitem";
 import Newnote from "./Newnote";
 function Notes() {
+  let navigate = useNavigate();
   let context = useContext(NotesContext);
   let { Notes, fetchNotes } = context;
   useEffect(() => {
-    fetchNotes();
+    if (localStorage.getItem("auth-token")) {
+      fetchNotes();
+    } else {
+      navigate("/login");
+    }
   }, []);
 
   return (
@@ -15,9 +21,13 @@ function Notes() {
         <Newnote />
         <h2 className="my-2 text-2xl font-bold text-center">All Notes</h2>
         <div className="my-4 flex flex-wrap justify-center items-center gap-8">
-          {Notes.map((note) => {
-            return <Noteitem key={note._id} note={note} />;
-          })}
+          {Notes.length > 0 ? (
+            Notes.map((note) => {
+              return <Noteitem key={note._id} note={note} />;
+            })
+          ) : (
+            <div>No Notes Found</div>
+          )}
         </div>
       </div>
     </>
