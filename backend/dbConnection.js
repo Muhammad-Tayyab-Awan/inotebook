8d37cce1d46d3a891c1361e98ecb795dc98b0de6
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-09-30 21:00:54 +0500

    added mongodb_url

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -1,7 +1,8 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+const mongo_URL = process.env.DB_URL;
 function main() {
-    mongoose.connect('mongodb://127.0.0.1:27017/inotebook').then(() => {
-        console.log("Connected To DB");
-    });
+  mongoose.connect(mongo_URL).then(() => {
+    console.log("Connected To DB");
+  });
 }
 export default main;
