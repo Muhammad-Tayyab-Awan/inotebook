8f780c6a6d897736b6509d2f399e69ce61c5bf76
Muhammad Tayyab <mtayyabawan1918@gmail.com>
2024-10-03 11:09:29 +0500

    Chaged Width of Mai  Container  for small devices

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,11 +9,11 @@ function About(props) {
   return (
     <>
       <div className="min-h-[calc(100vh-9.5rem)] bg-yellow-500 dark:bg-[#776e6e] pt-16 pb-10">
-        <div className="mx-auto w-[75%] text-[#111827] dark:text-white py-8 px-4">
+        <div className="mx-auto w-11/12 md:w-[75%] text-[#111827] dark:text-white py-8 px-4">
           <h1 className="text-2xl font-bold mb-4 text-center">
             About iNotebook
           </h1>
-          <p className="text-lg mb-4 w-3/4 mx-auto">
+          <p className="text-lg mb-4 w-11/12 md:w-3/4 mx-auto">
             Welcome to <strong>iNotebook</strong>, your go-to platform for
             managing notes in the cloud. Whether you need to jot down ideas,
             create task lists, or store important information, iNotebook makes
@@ -23,7 +23,7 @@ function About(props) {
             safe and organized.
           </p>
           <h2 className="text-xl font-bold mb-4 text-center">Why iNotebook?</h2>
-          <p className="text-lg mb-4 w-3/4 mx-auto">
+          <p className="text-lg mb-4 w-11/12 md:w-3/4 mx-auto">
             <strong>iNotebook</strong> was designed to be more than just a
             note-taking app—it&apos;s built with a focus on simplicity and user
             experience. Unlike other apps, the clean, intuitive UI allows you to
@@ -32,7 +32,7 @@ function About(props) {
             using <strong>React.js</strong> and <strong>Tailwind CSS</strong>.
           </p>
           <h2 className="text-xl font-bold mb-4 text-center">Who We Are?</h2>
-          <p className="text-lg mb-4 w-3/4 mx-auto">
+          <p className="text-lg mb-4 w-11/12 md:w-3/4 mx-auto">
             <strong>iNotebook</strong> is the brainchild of Muhammad Tayyab, a
             passionate front-end developer who created this project as part of
             their portfolio. As a developer with expertise in{" "}
@@ -46,7 +46,7 @@ function About(props) {
           <h2 className="text-xl font-bold mb-4 text-center">
             The Vision Behind iNotebook
           </h2>
-          <p className="text-lg mb-4 w-3/4 mx-auto">
+          <p className="text-lg mb-4 w-11/12 md:w-3/4 mx-auto">
             The vision behind <strong>iNotebook</strong> is to provide a simple,
             secure, and efficient way for users to manage their notes in the
             cloud. Whether you&apos;re a student, professional, or someone who
